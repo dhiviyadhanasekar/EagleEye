3e157596f118b43e7deb9e98d0e64e2a28d38fe1
Dhiviya Dhanasekar (Work) <dhiviyad@yahoo-inc.com>
2016-10-29 11:36:08 -0700

    My first iOS APP

--- EagleEye/ViewController.swift
+++ EagleEye/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue : Int = 0
+    var targetValue : Int = 0
+    //@IBOutlet similar to @IBAction. @IBOutlet links vars to UI. @IBAction links the func to the UI
+    @IBOutlet weak var slider: UISlider!
+    
+    func startNewRound(){
+        currentValue = 50
+        targetValue = 1 + Int(arc4random_uniform(100))
+        slider.value = Float(currentValue)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+//        currentValue = lroundf(slider.value)
+//        targetValue = 1 + Int(arc4random_uniform(100))
+        
+        startNewRound()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showAction(){
+        let message = "The value of the slider is \(currentValue)\n" + "The target value is \(targetValue)"
+        let alert = UIAlertController(title: "Hi There!", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK!", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        currentValue = lroundf(slider.value)
+//        print("The value of the slider is now: \(slider.value)")
+        
+    }
+    
+    
 
 }
 
