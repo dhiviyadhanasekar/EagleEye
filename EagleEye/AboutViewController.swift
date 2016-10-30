eb3f543abe058208424ba64bcf17352c0d6a52c8
Dhiviya Dhanasekar (Work) <dhiviyad@yahoo-inc.com>
2016-10-29 18:56:22 -0700

    Info page and game completion

---
+++ EagleEye/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  EagleEye
+//
+//  Created by Dhiviya Dhanasekar on 10/29/16.
+//  Copyright © 2016 Dhiviya Dhanasekar. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
--- EagleEye/ViewController.swift
+++ EagleEye/ViewController.swift
@@ -12,13 +12,32 @@ class ViewController: UIViewController {
     
     var currentValue : Int = 0
     var targetValue : Int = 0
+    var score : Int = 0
+    var round : Int = 0
     //@IBOutlet similar to @IBAction. @IBOutlet links vars to UI. @IBAction links the func to the UI
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     func startNewRound(){
         currentValue = 50
         targetValue = 1 + Int(arc4random_uniform(100))
         slider.value = Float(currentValue)
+        round += 1
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewGame(){
+        score = 0
+        round = 0
+        startNewRound()
+        updateLabels()
     }
 
     override func viewDidLoad() {
@@ -29,6 +48,7 @@ class ViewController: UIViewController {
 //        targetValue = 1 + Int(arc4random_uniform(100))
         
         startNewRound()
+        updateLabels()
         
     }
 
@@ -36,19 +56,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func startOver(){
+        startNewGame()
+    }
 
     @IBAction func showAction(){
-        let message = "The value of the slider is \(currentValue)\n" + "The target value is \(targetValue)"
-        let alert = UIAlertController(title: "Hi There!", message: message, preferredStyle: .alert)
+        
+        let difference = abs(targetValue-currentValue)
+        var points = 100 - difference
+        
+        let title: String
+        if difference == 0 {
+            
+            title = "You got the eagle's eyes!"
+            points += 100
+            
+        } else if difference < 5 {
+            
+            title = "You almost had it!"
+            points += 50
+            
+        } else if difference < 10 {
+            title = "Pretty Good!"
+        } else {
+            title = "Not so good.... but try again."
+        }
+        
+        score += points
+        let message = "You score points \(points)"
+        
+//        let message = "The value of the slider is \(currentValue)\n" +
+//                            "The target value is \(targetValue)\n" +
+//                            "You score points \(points)"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK!", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         startNewRound()
+        updateLabels()
+        
     }
     
     @IBAction func sliderMoved(_ slider: UISlider){
         currentValue = lroundf(slider.value)
+        updateLabels()
 //        print("The value of the slider is now: \(slider.value)")
         
     }
